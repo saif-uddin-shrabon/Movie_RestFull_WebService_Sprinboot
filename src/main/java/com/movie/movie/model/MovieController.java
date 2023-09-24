9c9a34a75004b7abcf853858cce43b2a6d3e868a
saif-uddin-shrabon <saif06065@gmail.com>
2023-09-24 21:52:07 +0600

    Complete

--- src/main/java/com/movie/movie/model/MovieController.java
+++ src/main/java/com/movie/movie/model/MovieController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import static com.movie.movie.model.DB.movie;
@@ -30,10 +31,46 @@ public class MovieController {
     }
 
 
+    @PutMapping("/updateMovie/{imdbId}")
+    public ResponseEntity<Bean> updateMovie(@PathVariable String imdbId, @RequestBody Bean updatedMovie) {
+
+        Bean existingMovie = null;
+        for (Bean movieItem : movie) {
+            if (movieItem.getImdbId().equals(imdbId)) {
+                existingMovie = movieItem;
+                break;
+            }
+        }
+
+        if (existingMovie != null) {
+
+            existingMovie.setTitle(updatedMovie.getTitle());
+            existingMovie.setDescription(updatedMovie.getDescription());
+            existingMovie.setRating(updatedMovie.getRating());
+            existingMovie.setGenre(updatedMovie.getGenre());
+            existingMovie.setYear(updatedMovie.getYear());
+
+            return new ResponseEntity<>(existingMovie, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/deleteMovie/{imdbId}")
+    public ResponseEntity<String> deleteMovie(@PathVariable String imdbId) {
+
+        Iterator<Bean> iterator = movie.iterator();
+        while (iterator.hasNext()) {
+            Bean movieItem = iterator.next();
+            if (movieItem.getImdbId().equals(imdbId)) {
+                iterator.remove();
+                return new ResponseEntity<>("Movie with IMDb ID " + imdbId + " deleted successfully", HttpStatus.OK);
+            }
+        }
 
 
-
-
+        return new ResponseEntity<>("Movie with IMDb ID " + imdbId + " not found", HttpStatus.NOT_FOUND);
+    }
 
 
 }
